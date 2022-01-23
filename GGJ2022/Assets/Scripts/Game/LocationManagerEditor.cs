156af8109631de4d0b0d71de133618c9c36b5191
Michael Lee <michael.hl.lee@gmail.com>
2022-01-23 12:58:45 -0800

    added a simple tool to make it easier to view Locations with the Main Camera.

--- GGJ2022/Assets/Scripts/Game/LocationManagerEditor.cs
+++ GGJ2022/Assets/Scripts/Game/LocationManagerEditor.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
+using UnityEditor.EditorTools;
+
 [CustomEditor(typeof(LocationManager))]
 public class LocationManagerEditor : Editor
 {
@@ -32,6 +34,59 @@ public class LocationManagerEditor : Editor
                 myTarget.SetLocation(location);
             }
         }
+    }
+}
+
+[EditorTool("Location Tool", typeof(Camera))]
+class LocationTool : EditorTool {
+    // Serialize this value to set a default value in the Inspector.
+    [SerializeField]
+    Texture2D m_ToolIcon;
+
+    GUIContent m_IconContent;
+
+    List<Location> locations = new List<Location>();
+
+    private void Awake() {
+        m_ToolIcon =  AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Textures/location_tool.png");
+        locations.Clear();
+        var objs = FindObjectsOfType<Location>();
+        foreach (var loc in objs) {
+            locations.Add((Location)loc);
+        }
+    }
+    void OnEnable()
+    {
+        m_IconContent = new GUIContent()
+        {
+            image = m_ToolIcon,
+            text = "Platform Tool",
+            tooltip = "Platform Tool"
+        };
+    }
+
+    public override GUIContent toolbarIcon
+    {
+        get { return m_IconContent; }
+    }
+    // This is called for each window that your tool is active in. Put the functionality of your tool here.
+    public override void OnToolGUI(EditorWindow window)
+    {
+        Camera cam = (Camera)target;
+        Handles.BeginGUI();
+        {
+            GUILayout.BeginArea(new Rect(0, 0, 100, 200), new GUIStyle("box"));
+            {
+                foreach (var location in locations) {
+                    if (GUILayout.Button(location.name)) {
+                        cam.transform.position = location.transform.position;
+                        cam.transform.rotation = location.transform.rotation;
+                    }
+                }
+            }
+            GUILayout.EndArea();
+        }
+        Handles.EndGUI();
     }
 }
 #endif
