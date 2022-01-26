1c0b61f099320ddd58cf1f48a198123cc7f98857
Arthur <arthur.littlefield@gmail.com>
2022-01-25 21:07:39 -0800

    add rocker

---
+++ GGJ2022/Assets/Rocker.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocker : MonoBehaviour
+{
+    public bool ShouldRock;
+    private Rigidbody rb;
+    public Vector3 force;
+    public float timeToRock = 10;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeToRock)
+        {
+            ShouldRock = true;
+            timer = 0;
+        }
+        
+        if (ShouldRock)
+        {
+            ShouldRock = false;
+            Rock();
+        }
+    }
+
+    public void Rock()
+    {
+        rb.AddTorque(force, ForceMode.Force);
+    }
+}
